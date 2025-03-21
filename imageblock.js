a691f1614fc06b3e7757496bcb177d180c1d9c4b
Brent Durksen <brentdurksen@gmail.com>
2025-03-20 21:11:05 -0600

    blur images and run only in js

--- imageblock.js
+++ imageblock.js
@@ -1,6 +1,104 @@
-const blockBackgroundImages = document.createElement("style");
-blockBackgroundImages.textContent = `* { background-image: none !important; }`;
+/*
+ * TODOs:
+ * - [ ] Block images in iFrames
+ * - [ ] Make background images grayscale
+ * - [ ] Handle ::before and ::after pseudo-elements with content urls
+ * - [ ] GitHub fails on navigating to new page
+ */
 
+const blockStyle =
+  "* { background-image: none !important; } img, svg, video { opacity: 0.2 !important; filter: grayscale(100%) blur(50px) !important; }";
+const grayscaleStyle =
+  "* { background-image: none !important; } img, svg, video { filter: grayscale(100%) !important; }";
+const rootBlocker = document.createElement("style");
+rootBlocker.textContent = blockStyle;
+document.documentElement.insertBefore(
+  rootBlocker,
+  document.documentElement.firstChild
+);
+
+/*
+ * @returns {Array<ShadowRoot>} Array of ShadowRoots found below/contained
+ * @param {Node} ele - An element that we should search for ShadowRoots within.
+ *   within the given node, `ele`.
+ */
+function findShadowRoots(ele) {
+  if (ele.querySelectorAll) {
+    return [ele, ...ele.querySelectorAll("*")]
+      .filter((e) => !!e.shadowRoot)
+      .flatMap((e) => [e.shadowRoot, ...findShadowRoots(e.shadowRoot)]);
+  }
+  return [];
+}
+
+function findIFrames(ele) {
+  if (ele.querySelectorAll) {
+    return [ele, ...ele.querySelectorAll("iframe")]
+      .filter((e) => !!e.contentWindow)
+      .flatMap((e) => [e.contentWindow, ...findIFrames(e.contentWindow)]);
+  }
+  return [];
+}
+
+function makeImageBlocker(style) {
+  const blocker = document.createElement("style");
+  blocker.textContent = style;
+  return blocker;
+}
+
+const blockers = [];
+
+// add an observer to watch for new elements
+const observer = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    if (mutation.addedNodes.length > 0) {
+      for (const node of mutation.addedNodes) {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+          for (const root of findShadowRoots(node)) {
+            const shadowRootBlocker = makeImageBlocker(blockStyle);
+            root.appendChild(shadowRootBlocker);
+            blockers.push(shadowRootBlocker);
+          }
+        }
+      }
+    }
+  }
+});
+
+observer.observe(document.documentElement, {
+  childList: true,
+  subtree: true,
+});
+
+function blockImages() {
+  rootBlocker.textContent = blockStyle;
+  for (const root of findShadowRoots(document.documentElement)) {
+    const shadowRootBlocker = makeImageBlocker(blockStyle);
+    root.appendChild(shadowRootBlocker);
+    blockers.push(shadowRootBlocker);
+  }
+  for (const iframe of findIFrames(document.documentElement)) {
+    const iframeBlocker = makeImageBlocker(blockStyle);
+    iframe.document.documentElement.appendChild(iframeBlocker);
+    blockers.push(iframeBlocker);
+  }
+}
+
+function setGrayscaleFilter() {
+  rootBlocker.textContent = grayscaleStyle;
+  for (const blocker of blockers) {
+    blocker.textContent = grayscaleStyle;
+  }
+}
+
+function removeFilters() {
+  rootBlocker.textContent = "";
+  for (const blocker of blockers) {
+    blocker.textContent = "";
+  }
+}
+
+// Apply settings; default to blocking images
 function applySettings(settings) {
   const sortedSettings = Object.keys(settings).sort(
     (a, b) => b.length - a.length
@@ -9,29 +107,131 @@ function applySettings(settings) {
   const thisSiteSetting = sortedSettings.find((key) => {
     return tabUrl.startsWith(key);
   });
-  const docStyle = document.documentElement.style;
-  if (settings[thisSiteSetting]?.filter === "grayscale") {
-    docStyle.setProperty("--image-block-opacity", "initial");
-    blockBackgroundImages.remove();
-  } else if (settings[thisSiteSetting]?.filter === "all") {
-    docStyle.setProperty("--image-block-opacity", "initial");
-    docStyle.setProperty("--image-block-grayscale-filter", "initial");
+  const filter = settings[thisSiteSetting]?.filter;
+  if (filter === "grayscale") {
+    setGrayscaleFilter();
+  } else if (filter === "all") {
+    removeFilters();
   } else {
-    docStyle.setProperty("--image-block-opacity", "0");
-    docStyle.setProperty("--image-block-grayscale-filter", "grayscale(100%)");
-    document.head.appendChild(blockBackgroundImages);
+    blockImages();
   }
 }
 
+// Apply settings on page load
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", () => {
+    browser.storage.sync.get("imageBlock_blockImages").then((setting) => {
+      applySettings(JSON.parse(setting.imageBlock_blockImages) ?? {});
+    });
+  });
+}
+
+// Apply settings when receiving a message after change
 browser.runtime.onMessage.addListener((message) => {
   if (message.type === "blockImages") {
     applySettings(JSON.parse(message.settings) ?? {});
+  } else if (message.type === "blockImagesOverride") {
+    if (message.filter === "grayscale") {
+      setGrayscaleFilter();
+    } else if (message.filter === "all") {
+      removeFilters();
+    } else {
+      blockImages();
+    }
   }
 });
 
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", async () => {
-    const setting = await browser.storage.sync.get("imageBlock_blockImages");
-    applySettings(JSON.parse(setting.imageBlock_blockImages) ?? {});
-  });
+// document.querySelectorAll("*").forEach((element) => {
+//   const beforeStyle = window.getComputedStyle(element, "::before");
+//   const afterStyle = window.getComputedStyle(element, "::after");
+
+//   if (beforeStyle.content.startsWith("url(")) {
+//     element.style.setProperty("--before-content", "url(none)");
+//   }
+
+//   if (afterStyle.content.startsWith("url(")) {
+//     element.style.setProperty("--after-content", "url(none)");
+//   }
+// });
+
+const imageExtensions = [
+  "bmp",
+  "gif",
+  "ico",
+  "jpeg",
+  "jpg",
+  "jfif",
+  "jxl",
+  "pjpeg",
+  "pjp",
+  "png",
+  "svg",
+  "tif",
+  "tiff",
+  "webp",
+  "apng",
+  "avif",
+];
+
+// Function to find all CSS properties with URLs
+function findCSSPropertiesWithURLs() {
+  const elements = document.querySelectorAll("*"); // Select all elements
+  const urlProperties = new Set(); // Use a Set to avoid duplicates
+
+  for (const element of elements) {
+    const computedStyle = window.getComputedStyle(element); // Get computed styles
+
+    for (let i = 0; i < computedStyle.length; i++) {
+      const property = computedStyle[i];
+      const value = computedStyle.getPropertyValue(property);
+
+      // Check if the property value contains a URL
+      // and an extension that matches an image
+      if (
+        value.includes("url(") &&
+        imageExtensions.some((ext) => value.includes(`.${ext}`))
+      ) {
+        urlProperties.add(`${property}: ${value}`); // Add to the Set
+
+        // change style to remove the url
+        element.style.setProperty(property, "none");
+      }
+    }
+  }
+  return Array.from(urlProperties); // Convert Set to Array
 }
+
+// Function to find all CSS properties with URLs in stylesheets
+// function findCSSPropertiesWithURLs() {
+//   const urlProperties = new Set(); // Use a Set to avoid duplicates
+
+//   // Loop through all stylesheets
+//   for (const sheet of document.styleSheets) {
+//     try {
+//       // Loop through all rules in the stylesheet
+//       for (const rule of sheet.cssRules) {
+//         if (rule.style) {
+//           // Loop through all properties in the rule
+//           for (let i = 0; i < rule.style.length; i++) {
+//             const property = rule.style[i];
+//             const value = rule.style.getPropertyValue(property);
+
+//             // Check if the property value contains a URL
+//             // and an extension that matches an image
+//             if (
+//               value.includes("url(") &&
+//               imageExtensions.some((ext) => value.includes(`.${ext}`))
+//             ) {
+//               urlProperties.add(`${property}: ${value}`); // Add to the Set
+//             }
+//           }
+//         }
+//       }
+//     } catch (e) {
+//       // Catch security errors for cross-origin stylesheets
+//       console.warn(`Unable to access stylesheet: ${sheet.href}`, e);
+//     }
+//   }
+
+//   return Array.from(urlProperties); // Convert Set to Array
+// }
--- popup/popup.js
+++ popup/popup.js
@@ -1,76 +1,78 @@
 let settings = {};
-let tabUrl = "";
+let currentTab;
+
+// Get the current tab URL
 browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-  tabUrl = tabs[0].url;
+  currentTab = tabs[0];
 });
 
+// Get saved settings for the current site
 browser.storage.sync.get("imageBlock_blockImages").then((result) => {
   settings = JSON.parse(result.imageBlock_blockImages) ?? {};
   const sortedSettingsKeys = Object.keys(settings).sort(
     (a, b) => b.length - a.length
   );
-  const thisSiteKey = sortedSettingsKeys.find((key) => tabUrl.startsWith(key));
+  const thisSiteKey = sortedSettingsKeys.find((key) =>
+    currentTab.url.startsWith(key)
+  );
   if (thisSiteKey) {
-    // set radio buttons to saved value
-    const filters = document.getElementsByName("filter");
-    filters.forEach((filter) => {
-      if (filter.value === settings[thisSiteKey].filter) {
-        filter.checked = true;
-      }
-    });
-    const target = document.getElementsByName("target");
-    target.forEach((target) => {
-      if (target.value === settings[thisSiteKey].target) {
-        target.checked = true;
-      }
-    });
+    const filterRadio = document.getElementById(
+      `filter_${settings[thisSiteKey].filter}`
+    );
+    const targetRadio = document.getElementById(
+      `target_${settings[thisSiteKey].target}`
+    );
+    if (filterRadio && targetRadio) {
+      filterRadio.checked = true;
+      targetRadio.checked = true;
+    }
   }
 });
 
+// Save the settings to storage and send a message to all tabs to update the settings
 function saveSettings() {
   browser.storage.sync.set({
     imageBlock_blockImages: JSON.stringify(settings),
   });
   browser.tabs.query({}).then((tabs) => {
-    tabs.forEach((tab) => {
+    for (const tab of tabs) {
       browser.tabs.sendMessage(tab.id, {
         type: "blockImages",
         settings: JSON.stringify(settings),
       });
-    });
+    }
   });
-  window.close();
 }
 
-const form = document.getElementById("optionsForm");
-form.addEventListener("submit", function (event) {
+// Handle form submit
+function handleFormSubmit(event) {
   event.preventDefault();
   const formData = new FormData(form);
   const filter = formData.get("filter");
   const target = formData.get("target");
-  const urlTarget = target === "domain" ? new URL(tabUrl).origin : tabUrl;
+  const urlTarget =
+    target === "domain" ? new URL(currentTab.url).origin : currentTab.url;
   settings[urlTarget] = {
     filter,
     target,
   };
   saveSettings();
-});
+  window.close();
+}
 
-// // set value to saved value
-// browser.storage.sync.get("imageBlock_blockImages").then((result) => {
-//   blockImageCheckbox.checked = result.imageBlock_blockImages;
-// });
+// Add event listener to form submit
+const form = document.getElementById("optionsForm");
+form.addEventListener("submit", handleFormSubmit);
 
-// blockImageCheckbox.addEventListener("change", (event) => {
-//   browser.storage.sync.set({ imageBlock_blockImages: event.target.checked });
-//   console.log("blockImages", event.target.checked);
-//   // send message to content script in all tabs
-//   browser.tabs.query({}).then((tabs) => {
-//     tabs.forEach((tab) => {
-//       browser.tabs.sendMessage(tab.id, {
-//         type: "blockImages",
-//         blockImages: event.target.checked,
-//       });
-//     });
-//   });
-// });
+// Listen for override button clicks
+for (const override of ["none", "grayscale", "all"]) {
+  document
+    .getElementById(`override_${override}`)
+    .addEventListener("click", () => {
+      browser.tabs.sendMessage(currentTab.id, {
+        type: "blockImagesOverride",
+        filter: override,
+      });
+      window.close();
+    });
+}
